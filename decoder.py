e8d4a210295c58c29c50801a23aaf34cf03e4618
sinanm89 <sinanm89@gmail.com>
2017-06-17 23:41:18 -0400

    came as far as the matcher for first pattern but found out that Knuth-Morris-Pratt is better so Im transitioning master to that."

--- decoder.py
+++ decoder.py
@@ -1,82 +1,77 @@
-# def replacement(_input, inquiry, key=None):
-#     if key == _input:
+"""Decode reddit megalinks using the bayer moore algorithm with dynamic search patterns."""
+# as per http://www.personal.kent.edu/~rmuhamma/Algorithms/MyAlgorithms/StringMatch/boyerMoore.htm
+#  Knuth-Morris-Pratt is a better approach to this problem. We're essentially searching for the last
+# character of the pattern (a binary string?) so I will follow this educational page.
+
+# also how hard is it to explain this algorithm with this?!:
+# BOYER_MOORE_MATCHER (T, P)
+
+# Input:    Text with n characters and Pattern with m characters
+# Output: Index of the first substring of T matching P
+
+# Compute function last
+# i ← m-1
+# j ← m-1
+# Repeat
+#     If P[j] = T[i] then
+#         if j=0 then
+#             return i        // we have a match
+#         else
+#             i ← i -1
+#             j ← j -1
+#     else
+#         i ← i + m - Min(j, 1 + last[T[i]])
+#         j ← m -1
+# until i > n -1
+# Return "no match"
+# # Honestly...
+search_strings = ['link:', 'password:', 'replace ', 'add ']
 
 
-#     return output
-
-# def shift(input, inquiry):
-#     return output
-
-# # find character \n indexes
-# # see if each index corresponds to a case
-
-# def string_cases(input_string):
-#     k = len(input_string)
-#     char = input_string[0]
-#     check_case(input_string)
-
-#     if char is not " ":
-#         output, word, key = string_cases(input_string[k/2:])
-#         output, word, key = string_cases(input_string[:k/2])
-
-#     char = input_string[0]
-#     for i, char in enumerate(input_string):
-#         if char == " ":
-#             continue
-#         if char == "\n":
-#             output, word, key = string_cases(input_string[i:])
-# #
-# # KMP_SEARCH
-# def kmp_search(input_string, char):
-#     m = 0
-#     i = 0
-#     T = []
-#     inp_string_length = len(input_string)
-#     while m + i < inp_string_length:ter
-#         if input_string[i] == case[m]
-#         if
-
-# hash_table = {
-#     ' ': (0, [False]),
-#     '\n': (1, [False]),
-#     '!': (1, ['!']),
-#     '/': (2, ['#', True]),
-#     '#': (3, ['!', True]),
-#     ':': (4, [':']),
-#     'a': (5, ['c', 'd', 's']),
-#     'c': (6, ['e']),
-#     'd': (7, ['d', ':']),
-#     'e': (8, ['p', ':']),
-#     'i': (9, ['n']),
-#     'k': (10, [':']),
-#     'l': (11, ['a']),
-#     'n': (12, ['k']),
-#     'p': (13, ['a', 'l']),
-#     'r': (14, ['d', 'e']),
-#     's': (15, ['s', 'w']),
-#     'w': (16, ['o']),
-# }
-
-search_strings = ['link:', 'password:', 'replace', 'add']
+def get_link(input_string, index, output_link=None):
+    """Get the link string."""
+    if input_string[index] == '\n':
+        return output_link
+    elif output_link is not None:
+        output_link += input_string[index]
+        return get_link(input_string[index + 1:], 1, input_string[index + 1])
+    elif input_string[index] == '!':
+        return get_link(input_string[index + 1:], 1, input_string[index + 1])
+    elif input_string[index] == '#':
+        return get_link(input_string[index + 1:], 0)
+    # elif input_string[index] == ' ':
+        # return get_link(input_string[index + 1:], index)
 
 
-# def trigger_command(command):
-#     # do the action of the command
-#     # remove command pathways after possible commands are exhausted.
-#     # re-calculate the character weight table
-#     return output
+def get_password(input_string, index, output_link=None):
+    """Get the password string."""
+    if input_string[index] == '\n':
+        return output_link
+    elif output_link is not None:
+        output_link += input_string[index]
+        return get_link(input_string[index + 1:], 1, input_string[index + 1])
+    elif input_string[index] == '!':
+        return get_link(input_string[index + 1:], 1, input_string[index + 1])
+    else:
+        print '===============wow exception'
+        raise Exception
 
 
-class last_occurrence(object):
+class LastOccurance(object):
+
     """Last occurrence functor."""
 
     def __init__(self, pattern, alphabet):
-        """Generate a dictionary with the last occurrence of each alphabet
+        """
+        Generate a dictionary with the last occurrence of each alphabet
         letter inside the pattern.
 
         Note: This function uses str.rfind, which already is a pattern
         matching algorithm. There are more 'basic' ways to generate this
-        dictionary."""
+        dictionary.
+
+        pre-process the string?
+        """
         self.occurrences = dict()
         for letter in alphabet:
             self.occurrences[letter] = pattern.rfind(letter)
@@ -97,82 +92,75 @@ def boyer_moore_alg(input_string):
     '''
 
     # find all distinct characters in a string
+    pattern = 'link:'
     alphabet = set(input_string)
-    pattern = search_strings[0]
-    last = last_occurrence(pattern, alphabet)
-
-    n = len(input_string)
-    m = len(pattern)
-
-    inp_index = n - 1
-    search_index = m - 1
-
-    start_appending = False
-#
-    alphabet = set(input_string)
-    last = last_occurrence(pattern, alphabet)
+    last = LastOccurance(pattern, alphabet)
     pattern_len = len(pattern)
     n = len(input_string)
     i = pattern_len - 1  # input_string index
+    # if link: is pattern start at 5th index and search backwards
     j = pattern_len - 1  # pattern index
+    # if pattern is link: start from its end as well
 
+    data = {
+        'url': None,
+        'password': None
+    }
     while i < n:
+        # find the : character
         if input_string[i] == pattern[j]:
-            # input_string[i] == 'k':
-            #     get_link(input_string[i+1])
-            # print '-'*90; print '-'*90; import ipdb; ipdb.set_trace(context=13)  # breakpoint 9547f268  noqa  //
+            # lin(k)
+            if input_string[i - 1] == 'k':
+                print 'found link ', i
+                link = get_link(input_string[i:], 0)
+                data['url'] = link
+                pattern = 'password:'
+                # reset the pattern index into the new pattern
+                j = len(pattern) - 1
+                i = i + len(pattern) - 1
+            # passwor(d)
+            if input_string[i - 1] == 'd':
+                # a (dd) - I know this sucks but add is easier to write than concatonate.
+                if input_string[i] == ':':
+                    password = get_password(input_string[i:], 0)
+                    data['password'] = password
+                    pattern = 'add '
+                    # reset pattern - this ones experimental.
+                    j = len(pattern) - 1
+                    i = i + len(pattern) - 1
+                    # print data
+                elif input_string[i] == ' ':
+                    # do addition decode
+                    return -9000
+            # replac(e)
+            if input_string[i - 1] == 'e':
+                # do replace
+                return -80000
             if j == 0:
                 return i
             else:
+                print 'else j = ', j
+                print 'else i = ', i
                 i -= 1
                 j -= 1
         else:
             l = last(input_string[i])
+            # 4 + 4 + 1
+            print '-'*90; print '-'*90; import ipdb; ipdb.set_trace(context=13)  # breakpoint 3c8d7843  noqa  //
+
             i = i + pattern_len - min(j, 1 + l)
             j = pattern_len - 1
+    print '&&&'*30
+    print pattern
+    print '-'*30
+    print input_string
+    print 'i: ',i
+    print 'j: ',j
     return -1
-    # while inp_index < n:
-    #     print inp_index
-    #     if input_string[inp_index] in ['\n', ' ', '!']:
-    #         if input_string[inp_index] == '!':
-    #             print 'found !'
-    #             start_appending = not(start_appending)
-    #     elif input_string[inp_index] == search_string[search_index]:
-    #         if search_index == 0:
-    #             return inp_index
-    #         else:
-    #             inp_index -= 1
-    #             search_index -= 1
-    #     else:
-    #         l = last(input_string[search_index])
-    #         inp_index = search_index + m - min(search_index, 1 + l)
-    #         search_index = m - 1
-    # return -1
-
-
-    # k = len(input_string)
-    # char = input_string[index]
-    # char_weight, next_characters = hash_table.get(char, (-1, None))
-    # if char_weight < 0:
-    #     if maybe_command:
-    #         decoded_string.append(char)
-    #     # add or pass
-    # continue_command = next_characters[0]
-    # trigger = '!'
-    # if char == trigger:
-    #     output = trigger_command(maybe_command,)
-
-    # while continue_command and char is not trigger:
-    #     m = index + 1
-    #     if m > k:
-    #         break
-    #     char = input_string[m]
-    #     # O(m) > max length of the hash table maps
-    #     if char in next_characters:
-    #         break
 
 
 def main():
+    """Main funciton."""
     input_string = (
         'bullshit\ngreeting\nlink: /#!tmB3WbZQ\nreplace W with T\npossible-bullshit\npassword: '
         '!i2_r2FsdZveTGlCVN-gwI2cI4YQnCX0Kq1YaJUJDgQU\nbullshit\ngood\nfucking\n\\/\/bye.'
